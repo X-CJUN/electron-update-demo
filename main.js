a1fdb48ee4e05c3daee40986a8236a571228b47a
Honye <hongye.jun@qq.com>
2019-12-20 17:31:09 +0800

    feat: 检测更新

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const log  = require('electron-log')
 let win
 let menu
 
+let hasRegistListner = false
+// autoUpdater.autoDownload = false
+
 function createWindow() {
     win = new BrowserWindow({
         width: 800,
@@ -20,7 +23,7 @@ function createWindow() {
 // Logging
 autoUpdater.logger = log
 autoUpdater.logger.transports.file.level = 'info'
-log.info('App starting...')
+log.info('应用启动中...')
 
 // Define the menu
 let template = []
@@ -30,34 +33,52 @@ if (process.platform === 'darwin') {
         label: name,
         submenu: [
             {
-                label: 'About ' + name,
+                key: 'about',
+                label: '关于' + name,
                 role: 'about'
             },
             {
-                label: 'Restart and Install Update',
-                visible: false,
+                key: 'check',
+                label: '检查更新',
                 click() {
-                    app.quitAndInstall()
-                }
-            },
-            {
-                label: 'Check for Update',
-                click() {
+                    // 应用启动检测更新，无更新时不注册此事件
+                    if (!hasRegistListner) {
+                        autoUpdater.on('update-not-available', (ev, info) => {
+                            dialog.showMessageBox({
+                                type: 'warning',
+                                buttons: ['确定'],
+                                message: '无可用更新',
+                                title: '无可用更新',
+                                detail: `Version ${app.getVersion()} 已是最新版本`
+                            }, () => {})
+                        })
+                        hasRegistListner = true
+                    }
                     autoUpdater.checkForUpdates()
                 }
             },
             {
-                label: 'Checking for Update',
+                key: 'checking',
+                label: '检查更新中',
                 visible: false,
                 enabled: false,
             },
             {
-                label: 'Downloading Update',
+                key: 'downloading',
+                label: '下载更新中',
                 enabled: false,
                 visible: false,
             },
             {
-                label: 'Quit',
+                key: 'install',
+                label: '重启安装更新',
+                visible: false,
+                click() {
+                    autoUpdater.quitAndInstall()
+                }
+            },
+            {
+                label: '退出',
                 accelerator: "Command+Q",
                 click() {
                     app.quit()
@@ -90,52 +111,65 @@ app.on('activate', () => {
     }
 })
 
-autoUpdater.on('check-for-update', () => {
-    sendStatusToWindow('Checking for update...')
+autoUpdater.on('checking-for-update', () => {
+    sendStatusToWindow('检测更新中...')
     showUpdateMenuItem('checking')
 })
 autoUpdater.on('update-available', (ev, info) => {
-    sendStatusToWindow('Update available.')
-    showUpdateMenuItem('downloading')
+    sendStatusToWindow('有更新内容')
+    sendStatusToWindow(JSON.stringify({ ev, info }, 4))
+    dialog.showMessageBox({
+        type: 'info',
+        title: '更新提示',
+        message: ev.releaseNotes,
+        buttons: ['下载更新', '跳过'],
+    }, (index) => {
+        if (index === 0) {
+            autoUpdater.downloadUpdate()
+            showUpdateMenuItem('downloading')
+        } else {
+            showUpdateMenuItem('check')
+        }
+    })
 })
 autoUpdater.on('update-not-available', (ev, info) => {
-    sendStatusToWindow('Update not available.')
+    sendStatusToWindow('无可用更新')
     showUpdateMenuItem('no-update-available')
-    dialog.showMessageBox({
-        type: 'warning',
-        buttons: ['OK'],
-        message: 'No update available.',
-        title: 'No Update Available',
-        detail: `Version ${app.getVersion()} is the latest version.`
-    }, () => {})
 })
 autoUpdater.on('error', (ev, err) => {
-    sendStatusToWindow('Error in auto-updater.')
+    sendStatusToWindow('auto-updater 检测更新发生异常')
     showUpdateMenuItem('error')
     dialog.showMessageBox({
         type: 'warning',
-        buttons: ['OK'],
-        message: 'There was an error checking for updates.',
-        title: 'Update Error',
+        buttons: ['确定'],
+        message: '检测更新发生异常',
+        title: '更新异常',
         detail: err,
     }, () => {})
 })
 autoUpdater.on('download-progress', (ev, progressObj) => {
-    sendStatusToWindow('Download progress...')
+    sendStatusToWindow('下载进度')
+    sendStatusToWindow(JSON.stringify({ev, progressObj}, 4))
     showUpdateMenuItem('downloading')
 })
 autoUpdater.on('update-downloaded', (ev, info) => {
-    sendStatusToWindow('Update downloaded; will install in 5 seconds')
+    sendStatusToWindow('更新下载完成')
     showUpdateMenuItem('update-available')
+    dialog.showMessageBox({
+        type: 'info',
+        title: '更新下载完成',
+        message: '更新下载完成, 立即更新?',
+        buttons: ['立即更新', '稍等']
+    }, (index) => {
+        if (index === 0) {
+            autoUpdater.quitAndInstall()
+        }
+    })
 })
 
-// autoUpdater.on('update-downloaded', (ev, info) => {
-//     setTimeout(() => {
-//         autoUpdater.quitAndInstall()
-//     }, 5000);
-// })
 app.on('ready', function() {
-    // if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+    // autoUpdater.checkForUpdates()
+    autoUpdater.checkForUpdatesAndNotify()
 })
 
 function flattenMenuItems (menu) {
@@ -151,10 +185,10 @@ function flattenMenuItems (menu) {
 
 function showUpdateMenuItem (state) {
     const items = flattenMenuItems(menu)
-    const checkForUpdateItem = items.find(({label}) => label === 'Check for Update')
-    const checkingForUpdateItem = items.find(({label}) => label === 'Checking for Update')
-    const downloadingUpdateItem = items.find(({label}) => label === 'Downloading Update')
-    const installUpdateItem = items.find(({label}) => label === 'Restart and Install Update')
+    const checkForUpdateItem = items.find(({key}) => key === 'check')
+    const checkingForUpdateItem = items.find(({key}) => key === 'checking')
+    const downloadingUpdateItem = items.find(({key}) => key === 'downloading')
+    const installUpdateItem = items.find(({key}) => key === 'install')
 
     if (!checkForUpdateItem || !checkingForUpdateItem ||
         !downloadingUpdateItem || !installUpdateItem) return
@@ -177,7 +211,10 @@ function showUpdateMenuItem (state) {
         downloadingUpdateItem.visible = true
         break
       case 'update-available':
-        installUpdateItem.visible = true
+        // installUpdateItem.visible = true
+        checkForUpdateItem.visible = true
         break
+      default:
+            checkForUpdateItem.visible = true
     }
 }
